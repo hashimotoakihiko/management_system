97f22dc3b20e57f5f3dc36e9945c302964769322
橋本明彦 <58211196+hashimotoakihiko@users.noreply.github.com>
2021-12-21 11:36:40 +0900

    1221_route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//商品一覧画面を表示
+Route::get('/','App\Http\Controllers\BlogController@showList')->name
+('users');
+
+
+
+
+
+
+
+
+/*
+//ブログ一覧画面を表示
+Route::get('/','App\Http\Controllers\BlogController@showList')->name
+('blogs');
+
+//ブログ登録画面を表示
+Route::get('/blog/create','App\Http\Controllers\BlogController@showCreate')->name
+('create');
+
+//ブログ登録
+Route::post('/blog/store','App\Http\Controllers\BlogController@exeStore')->name
+('store');
+
+//ブログ詳細画面を表示
+Route::get('/blog/{id}','App\Http\Controllers\BlogController@showDetail')->name
+('show');
+
+//ブログ編集画面を表示
+Route::get('/blog/edit/{id}','App\Http\Controllers\BlogController@showEdit')->name
+('edit');
+Route::post('/blog/update','App\Http\Controllers\BlogController@exeUpdate')->name
+('update');
+*/
