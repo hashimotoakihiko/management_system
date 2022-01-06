e0295e2f2e7685adf1b5b0d15aa4f3f01cb6657f
hashimotoakihiko <ak-hashimoto@clinks.jp>
2022-01-07 01:49:03 +0900

    route_コンフリクト解消
    route_コンフリクト解消

--- routes/web.php
+++ routes/web.php
@@ -22,39 +22,3 @@ use Illuminate\Support\Facades\Route;
 // });
     
 Route::get('samplepost', 'Management_systemController@showList');
-
-
-
-
-//商品一覧画面を表示
-<<<<<<< HEAD
-// Route::get('/', function () {
-//   return view('list.blade.php');
-// });
-
-Route::GET('/',
-'App\Http\Controllers\Management_systemController@showList')->name
-('users');
-=======
-// Route::get('/','App\Http\Controllers\Management_systemController@showList')->name
-// ('users');
-
-
-// //商品登録画面を表示
-// Route::get('/management_system/create','App\Http\Controllers\Management_systemController@showCreate')->name
-// ('create');
-
-// //商品登録
-// Route::post('/management_system/store','App\Http\Controllers\Management_systemController@exeStore')->name
-// ('store');
-
-// //商品詳細画面を表示
-// Route::get('/management_system/{id}','App\Http\Controllers\Management_systemController@showDetail')->name
-// ('show');
-
-// //商品編集画面を表示
-// Route::get('/management_system/edit/{id}','App\Http\Controllers\Management_systemController@showEdit')->name
-// ('edit');
-// Route::post('/management_system/update','App\Http\Controllers\Management_systemController@exeUpdate')->name
-// ('update');
->>>>>>> 11450d0630e8205af33d5871e46182e732458938
