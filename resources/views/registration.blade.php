56bf9044d79673aaeccd11b676b12ab9d8c0589c
hashimotoakihiko <ak-hashimoto@clinks.jp>
2022-08-04 18:40:17 +0900

    password_form
    password_form
    JavaScriptでアラート処理

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -16,32 +16,92 @@
 
 
 <div class="content">
-<h2>新規ユーザー</h2>
+  <h2>新規ユーザー</h2>
 
-  <div class="name">
-    <label>ユーザー名:
-      <input type="text" name="name" placeholder="ユーザー名を入力">
-    </label>
-  </div>
+    <div class="name">
+      <label>ユーザー名:</label>
+        <input type="text" name="name" placeholder="ユーザー名を入力">
+    </div>
 
-  <div class="email">
-    <label>メールアドレス:
-      <input type="text" name="email" placeholder="メールアドレスを入力">
-    </label>
-  </div>
-  
-    <div class="pass">
-    <label>パスワード:
-      <input type="text" name="パスワード" placeholder="パスワードを入力">
-    </label>
-  </div>
+    <div class="email">
+      <label>メールアドレス:</label>
+        <input type="text" name="email" placeholder="メールアドレスを入力">
+    </div>
+    
 
-  <div class="pass">
-    <label>パスワード(確認用):
-      <input type="text" name="パスワード(確認用)" placeholder="パスワード(確認用)を入力">
-    </label>
-  </div>
+  <form onSubmit="return CheckEmail()">
+    <div class="pass1">
+      <label for="pass1">パスワード:</label>
+      <input type="password" id="pass1" name="パスワード" placeholder="パスワードを入力" required>
+    </div>
+
+    <div class="pass2">
+      <label for="pass1">パスワード(確認用):</label>
+      <input type="password" id="pass2" name="パスワード(確認用)" placeholder="パスワード(確認用)を入力" required>
+    </div>
+
+    <p>
+    <input type="submit" id="submit" name="register" value="登録">
+   </p>
+  </form>
+
+  <script language="JavaScript" type="text/javascript">
+<!--
+  function CheckEmail() {
+    //IE対応の為変更
+    //var mail = email_1.value; //メールフォームの値を取得
+    //var mailConfirm = emailConfirm_1.value; //メール確認用フォームの値を取得
+    var mail = document.getElementById("pass1").value; //メールフォームの値を取得
+    var mailConfirm = document.getElementById("pass2").value; //メール確認用フォームの値を取得
+    // パスワードの一致確認
+    if (mail != mailConfirm){
+      alert("パスワードと確認用パスワードが一致しません"); // 一致していなかったら、エラーメッセージを表示する
+      return false;
+    }else{
+      return true;
+    }
+  };
+// -->
+</script>
+
 </div>
+<!-- 
+<form onSubmit="return CheckEmail_1()">
+  <div>
+    <label for="email_1">メール:</label>
+  </div>
+  <div>
+    <input type="email" id="email_1" required>
+  </div>
+  <div>
+    <label for="emailConfirm_1">メール確認用:</label>
+  </div>
+  <div>
+    <input type="email" id="emailConfirm_1" required>
+  </div>
+  <p>
+    <input type="submit" id="submit">
+  </p>
+</form>
+
+<script language="JavaScript" type="text/javascript">
+
+  function CheckEmail_1() {
+    //IE対応の為変更
+    //var mail = email_1.value; //メールフォームの値を取得
+    //var mailConfirm = emailConfirm_1.value; //メール確認用フォームの値を取得
+    var mail = document.getElementById("email_1").value; //メールフォームの値を取得
+    var mailConfirm = document.getElementById("emailConfirm_1").value; //メール確認用フォームの値を取得
+    // パスワードの一致確認
+    if (mail != mailConfirm){
+      alert("パスワードと確認用パスワードが一致しません"); // 一致していなかったら、エラーメッセージを表示する
+      return false;
+    }else{
+      return true;
+    }
+  };
+//
+</script> -->
 
 <div class="footer">
   <div class="registration">
@@ -49,7 +109,7 @@
   </div>
   
   <div class="return">
-    <a href="http://localhost:8888/login">戻る</a>
+            <button onclick="location.href='login'">トップページへ戻る</button>
   </div>
 </div>
 
