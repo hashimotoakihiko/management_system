4725d8a8d0226eaa398a1a45b648c874006a4059
橋本明彦 <hashimotoakihiko@hashimotoakihikonoMacBook-Air.local>
2023-04-12 18:31:29 +0900

    修正
    修正

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Http\Requests\ProductRequest;
+// use Illuminate\Support\Facades\View;
 
 class ProductController extends Controller
 {
@@ -31,25 +32,109 @@ class ProductController extends Controller
 
     public function productshowList()
     {
-        $products = Product::all();
+        // $products = Product::all();
+        // $products = Product::where('id',1)->first();
+        // $products = Product::find(1);
+        $products = Product::get();
 
-       // dd($companies);
+    //    dd($products);
 
         return view ('product_list',
         ['products' => $products]);
       
     }
     
-    public function productsearch()
+
+    public function index(Request $request)
     {
-        $products = Product::where('id',1)->first();
-        // $data = ['msg' => '商品一覧', 'products' => $products];
 
-        //dd($companies);
+         /* テーブルから全てのレコードを取得する */
+        $products = Product::query();
+        // dd($products);
 
-        // return view('product_list', $data);
 
-        return view ('product_list',
-        ['products' => $products]);
+        /* キーワードから検索処理 */
+        $keyword = $request->input('keyword');
+        if(!empty($keyword)) {//$keyword　が空ではない場合、検索処理を実行します
+            // $products->where('company_id', 'LIKE', "%{$keyword}%")
+            // ->orwhereHas('products', function ($query) use ($keyword) {
+            //     $query->where('product_name', 'LIKE', "%{$keyword}%");
+            // })->get();
+            $products->where('product_name', 'LIKE', "%{$keyword}%")->get();
+            
+
+        }
+
+        /* ページネーション */
+        $products = $products->paginate(5);
+
+        return view('product_list', ['products' => $products]);
+
     }
+
+
+    
+    // public function index(Request $request)
+    // {
+    //     $keyword = $request->input('keyword');
+
+    //     $query = Product::query();
+
+    //     if(!empty($keyword)) {
+    //         $query->where('id', 'LIKE', "%{$keyword}%")
+    //             ->orWhere('product_name', 'LIKE', "%{$keyword}%");
+    //     }
+
+    //     $products = $query->get();
+
+    //     return view('product.index', compact('products', 'keyword'));
+    // }
+
+    // public function create()
+    // {
+    //     return view('product.create');
+    // }
+
+    // public function store(Request $request)
+    // {
+    //     $product = new Product();
+    //     $product->id = $request->id;
+    //     $product->company_id = $request->company_id;
+    //     $product->product_name = $request->product_name;
+    //     $product->price = $request->price;
+    //     $product->stock = $request->stock;
+    //     $product->comment = $request->comment;
+    //     $products->save();
+    //     return back();
+    // }
+
+    // //ここまで追加
+    // public function show(Product $product)
+    // {
+    //     return view('product.show',compact('product'));
+    // }
+
+
+
+
+    // public function getIndex(Request $rq)
+    // {
+    //     //キーワード受け取り
+    //     $products = $rq->input('keyword');
+    
+    //     //クエリ生成
+    //     $query = \App\Product::query();
+    
+    //     //もしキーワードがあったら
+    //     if(!empty($products))
+    //     {
+    //         $query->where('email','like','%'.$products.'%');
+    //         $query->orWhere('name','like','%'.$products.'%');
+    //     }
+    
+    //     // 全件取得 +ページネーション
+    //     $students = $query->orderBy('id','desc')->paginate(5);
+    //     return view('student.list')->with('students',$students)->with('keyword',$products);
+    // }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CompanieController;
 use App\Http\Controllers\ProductController;
+// use App\Http\Controllers\SaleController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,52 +25,50 @@ Route::get('/',
 Route::get('/login', [UserController::class,'showLogin'])->name
 ('showLogin');
 
-
-// Route::get('/product_list', [CompanieController::class,'showList'])->name
-// ('showlist');
-
-Route::post('/product_list', [CompanieController::class,'showList'])->name
-('showList');
-
-Route::get('/registration', [UserController::class,'new'])->name
-('new');
-
-Route::post('/registration', [UserController::class,'new'])->name
-('new');
-
-Route::get('/product_list', [CompanieController::class,'search'])->name
-('search');
-
-
-Route::get('/product_new', [ProductController::class,'product_new'])->name
-('product_new');
-
-Route::post('/product_new', [ProductController::class,'product_new'])->name
-('product_new');
-
-Route::get('/product_details', [ProductController::class,'product_details'])->name
-('product_details');
-
-Route::post('/product_details', [ProductController::class,'product_details'])->name
-('product_details');
-
-Route::get('/product_edit', [ProductController::class,'product_edit'])->name
-('product_edit');
-
-Route::post('/product_edit', [ProductController::class,'product_edit'])->name
-('product_edit');
-
-
-
+//商品一覧画面表示
 Route::get('/product_list', [ProductController::class,'productshowList'])->name
 ('productshowList');
 
 Route::post('/product_list', [ProductController::class,'productshowList'])->name
 ('productshowList');
 
-Route::get('/product_list', [ProductController::class,'productsearch'])->name
-('productsearch');
+//新規ユーザー登録画面表示
+Route::get('/registration', [UserController::class,'new'])->name
+('new');
 
+Route::post('/registration', [UserController::class,'new'])->name
+('new');
+
+//商品新規登録表示
+Route::get('/product_new', [ProductController::class,'product_new'])->name
+('product_new');
+
+Route::post('/product_new', [ProductController::class,'product_new'])->name
+('product_new');
+
+
+//商品詳細表示
+Route::get('/product_details', [ProductController::class,'product_details'])->name
+('product_details');
+
+Route::post('/product_details', [ProductController::class,'product_details'])->name
+('product_details');
+
+
+//商品編集表示
+Route::get('/product_edit', [ProductController::class,'product_edit'])->name
+('product_edit');
+
+Route::post('/product_edit', [ProductController::class,'product_edit'])->name
+('product_edit');
+
+//商品検索
+Route::get('/', [ProductController::class, 'index'])->name
+('products.index');
+Route::get('/index', [ProductController::class,'index'])->name
+('index');
+Route::get('/', [ProductController::class,'index'])->name
+('crud.index'); 
 
 
 
